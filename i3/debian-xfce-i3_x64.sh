1e7f95836d276caf809c5d1f4d6e4628d30f20ff
freddan88 <fredrik@leemann.se>
2022-01-29 23:42:09 +0100

    Update

--- i3/debian-xfce-i3_x64.sh
+++ i3/debian-xfce-i3_x64.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-URL_MINT_Y_THEME="http://packages.linuxmint.com/pool/main/m/mint-y-theme/mint-y-theme_1.2.3_all.deb"
-URL_MINT_Y_ICONS="http://packages.linuxmint.com/pool/main/m/mint-y-icons/mint-y-icons_1.5.9_all.deb"
-URL_MINT_X_ICONS="http://packages.linuxmint.com/pool/main/m/mint-x-icons/mint-x-icons_1.6.4_all.deb"
 URL_MONGODB_COMPASS="https://github.com/mongodb-js/compass/releases/download/v1.30.1/mongodb-compass_1.30.1_amd64.deb"
 URL_JETBRAINS_MONOFONT="https://github.com/JetBrains/JetBrainsMono/releases/download/v2.242/JetBrainsMono-2.242.zip"
 
@@ -24,6 +21,7 @@ get_i3_config() {
     echo "UPDATING i3 CONFIG" && sleep 2
     I3_CONFIG=$(curl -s https://raw.githubusercontent.com/freddan88/fredrik.linux.files/main/i3/config-i3-xfce.txt)
     mkdir -p $SUDO_USER_HOME/.config/i3 && echo $I3_CONFIG >$SUDO_USER_HOME/.config/i3/config
+    echo "Wrote new i3-configuration to: $SUDO_USER_HOME/.config/i3/config"
 }
 
 get_zsh_config() {
@@ -31,13 +29,14 @@ get_zsh_config() {
     echo "UPDATING ZSH CONFIG" && sleep 2
     ZSH_CONFIG=$(curl -s https://raw.githubusercontent.com/freddan88/fredrik.linux.files/main/shell/zshrc.txt)
     echo $ZSH_CONFIG >$SUDO_USER_HOME/.zshrc
+    echo "Wrote new zsh-configuration to: $SUDO_USER_HOME/.zshrc"
 }
 
 get_php_composer() {
     echo " "
     echo "UPDATING PHP COMPOSER" && sleep 2
     rm -f /usr/local/bin/composer
-    wget -q https://getcomposer.org/installer && php ./installer
+    wget -q https://getcomposer.org/installer && php ./installer >/dev/null
     mv composer.phar /usr/local/bin/composer && chmod 755 /usr/local/bin/composer
     echo "Installed the command: composer globally in: /usr/local/bin/composer"
 }
@@ -53,10 +52,24 @@ get_docker_compose() {
     echo "Installed the command: docker-compose globally in: /usr/local/bin/docker-compose"
 }
 
+run_config_lightdm_slick_greeter() {
+    echo " "
+    echo "UPDATING LIGHTDM SLICK GREETER" && sleep 2
+    LIGHTDM_SLICK_GREETER_CONFIG_FILE="/etc/lightdm/slick-greeter.conf"
+    echo "[Greeter]" >$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "background=/usr/share/backgrounds/linux-wallpaper-01.jpg" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "draw-user-backgrounds=false" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "theme-name=Arc-Dark" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "icon-theme-name=Elementary Xfce dark" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "activate-numlock=true" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "draw-grid=false" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
+    echo "Wrote new config in: $LIGHTDM_SLICK_GREETER_CONFIG_FILE"
+}
+
 install_all() {
     echo " "
     echo "INITIALIZE" && sleep 2
-    apt update -qq && apt install ca-certificates git unzip zip curl net-tools nano pwgen gnupg lsb-release -yqq
+    apt update -qq && apt install ca-certificates git unzip zip curl net-tools nano pwgen gnupg lsb-release gparted synaptic neofetch -yqq
 
     echo " "
     echo "ADDING KEYS AND REPOSITORIES" && sleep 2
@@ -81,35 +94,21 @@ install_all() {
     # wget -q https://dev.mysql.com/get/$LATEST_MYSQL && apt install ./mysql-apt-config_*_all.deb -yqq
 
     URL_LATEST_VSCODE="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-    wget -q $URL_LATEST_VSCODE
-    apt install code_*_amd64.deb -yqq
+    wget -q -O vscode_amd64.deb $URL_LATEST_VSCODE
+    apt install ./vscode_amd64.deb -yqq
 
     wget -q https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
     apt install ./dbeaver-ce_*_amd64.deb -yqq
 
-    wget -q $URL_MINT_Y_THEME
-    wget -q $URL_MINT_Y_ICONS
-    wget -q $URL_MINT_X_ICONS
-    apt install ./mint-y-icons_*_all.deb ./mint-x-icons_*_all.deb ./mint-y-theme_*_all.deb -yqq
-
     apt update -qq
-    apt install i3 ufw gufw fail2ban gimp thunderbird gparted synaptic neofetch nitrogen libreoffice compton sqlite3 libpcre3 libsodium23 insomnia spotify-client vlc -yqq
+    apt install ufw gufw fail2ban gimp arc-theme elementary-xfce-icon-theme thunderbird nitrogen libreoffice compton sqlite3 libpcre3 libsodium23 insomnia spotify-client -yqq
     apt install apache2 php php-{bcmath,cli,common,xdebug,curl,soap,gd,mbstring,mysql,opcache,readline,sqlite3,xml,zip,imagick,pear,cgi,phpseclib} libapache2-mod-php -yqq
     apt install imagemagick-common imagemagick-6-common imagemagick-6.q16 imagemagick-6.q16hdri libmagickcore-6.q16-6 libmagickwand-6.q16-6 libmagickwand-6.q16hdri-6 -yqq
-    apt install openssl libapache2-mpm-itk libmagickcore-6.q16hdri-3-extra libmagickcore-6.q16-6-extra ffmpeg ghostscript xfce4-screenshooter xfce4-appmenu-plugin -yqq
-    apt install docker-ce docker-ce-cli containerd.io rofi imagemagick stacer lightdm slick-greeter libappindicator3-0.1-cil libappindicator3-0.1-cil-dev ssh zsh -yqq
+    apt install openssl libapache2-mpm-itk libmagickcore-6.q16hdri-3-extra libmagickcore-6.q16-6-extra ffmpeg ghostscript xfce4-screenshooter xfce4-appmenu-plugin i3 -yqq
+    apt install docker-ce docker-ce-cli containerd.io rofi imagemagick stacer lightdm slick-greeter libappindicator3-0.1-cil libappindicator3-0.1-cil-dev ssh zsh vlc -yqq
 
     mkdir -p /usr/share/backgrounds
     wget -q https://img.wallpapersafari.com/desktop/1920/1080/95/51/LEps6S.jpg && mv LEps6S.jpg /usr/share/backgrounds/linux-wallpaper-01.jpg
-
-    LIGHTDM_SLICK_GREETER_CONFIG_FILE="/etc/lightdm/slick-greeter.conf"
-    echo "[Greeter]" >$LIGHTDM_SLICK_GREETER_CONFIG_FILE
-    echo "background=/usr/share/backgrounds/linux-wallpaper-01.jpg" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
-    echo "draw-user-backgrounds=false" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
-    echo "theme-name=Mint-Y-Dark" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
-    echo "icon-theme-name=Mint-Y-Dark" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
-    echo "activate-numlock=true" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
-    echo "draw-grid=false" >>$LIGHTDM_SLICK_GREETER_CONFIG_FILE
 
     LIGHTDM_MAIN_CONFIG_FILE="/etc/lightdm/lightdm.conf"
     echo " " >>$LIGHTDM_MAIN_CONFIG_FILE
@@ -124,6 +123,7 @@ install_all() {
     get_zsh_config
     get_php_composer
     get_docker_compose
+    run_config_lightdm_slick_greeter
 
     echo " "
 }
@@ -151,14 +151,19 @@ docker-compose)
     get_docker_compose
     ;;
 
+config-lightdm-slick-greeter)
+    run_config_lightdm_slick_greeter
+    ;;
+
 *)
     echo " "
-    echo "Usage: install | i3-config | zsh-config | php-composer | docker-compose"
+    echo "Usage: install | i3-config | zsh-config | php-composer | docker-compose | config-lightdm-slick-greeter"
     echo "./debian-xfce-i3_x64.sh install | Install everything and get the latest configurations"
     echo "./debian-xfce-i3_x64.sh i3-config | Download the latest i3-configuration from GitHub"
     echo "./debian-xfce-i3_x64.sh zsh-config | Download the latest zsh-configuration from GitHub"
     echo "./debian-xfce-i3_x64.sh php-composer | Download and install the latest php-composer script"
     echo "./debian-xfce-i3_x64.sh docker-compose | Download and install the latest docker-compose script"
+    echo "./debian-xfce-i3_x64.sh config-lightdm-slick-greeter | Write a new configuration-file for the login-screen"
     echo " "
     ;;
 
