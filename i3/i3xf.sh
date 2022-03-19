06f98853336a0af2d0b67f203dd4ae2436bc744f
freddan88 <fredrik@leemann.se>
2022-03-19 18:16:58 +0100

    add some more apt-install lines to script

--- i3/i3xf.sh
+++ i3/i3xf.sh
@@ -7,37 +7,45 @@ xfce4PanelProfilesMirrorLink="http://mirrors.kernel.org/ubuntu/pool/universe/x/x
 # When using i3-wm you can not use everything in xfce4-settings-manager so we hide those options
 unhandledSettings=(
   "xfce-session-settings.desktop"
-  "xfce-backdrop-settings.desktop" 
-  "xfce-workspaces-settings.desktop" 
-  "xfce-wmtweaks-settings.desktop" 
-  "xfce-wm-settings.desktop" 
+  "xfce-backdrop-settings.desktop"
+  "xfce-workspaces-settings.desktop"
+  "xfce-wmtweaks-settings.desktop"
+  "xfce-wm-settings.desktop"
 )
 
-function showConfigurationItems () {
+function showConfigurationItems() {
   for item in "${unhandledSettings[@]}"; do
     rm -f $userApplicationsDirectory/$item
   done
 }
 
-function hideConfigurationItems () {
+function hideConfigurationItems() {
   for item in "${unhandledSettings[@]}"; do
-    echo "Hidden=true" > $userApplicationsDirectory/$item
+    echo "Hidden=true" >$userApplicationsDirectory/$item
   done
 }
 
-function installEssentials () {
+function installEssentials() {
   # Create symbolic-link for gnome-control-center to let the global-menu work
   sudo ln -sf /bin/xfce4-settings-manager /usr/local/bin/gnome-control-center
-  sudo apt install ca-certificates curl wget gzip tar nano ssh git gnupg lsb-release gparted pwgen unzip zip net-tools vim -y
-  sudo apt install cups menulibre mugshot catfish lightdm slick-greeter xfce4-appmenu-plugin xfce4-screenshooter gnome-software synaptic stacer -y
-  
+  sudo apt install i3 i3status suckless-tools picom rofi playerctl xbacklight numlockx dos2unix xterm ftp tftpd-hpa net-tools fail2ban -y
+  sudo apt install ca-certificates zsh curl wget gzip bzip2 unzip zip tar ssh git lshw htop gnupg lsb-release pwgen nano vim ufw gufw -y
+  sudo apt install cups system-config-printer colord xiccd xarchiver exo-utils ntfs-3g exfat-utils dosfstools lightdm slick-greeter -y
+  sudo apt install menulibre mugshot catfish xfce4-appmenu-plugin xfce4-screenshooter gparted gnome-software synaptic stacer ffmpeg -y
+  sudo apt install gimp mirage thunderbird libreoffice ghostscript vlc openssl samba libpcre3 neofetch screenkey cpuid nitrogen -y
+  sudo apt install arc-theme gnome-icon-theme elementary-xfce-icon-theme debian-edu-artwork members cifs-utils screen -y
+  # apt install sqlite3 apache2 libapache2-mod-php php php-{bcmath,cli,common,xdebug,curl,soap,gd,mbstring,mysql,opcache,readline,sqlite3,xml,zip,imagick,pear,cgi,phpseclib} -y
+  # apt install libapache2-mpm-itk libsodium23 sqlitebrowser docker-ce docker-ce-cli containerd.io imagemagick imagemagick-common imagemagick-6-common imagemagick-6.q16 -y
+  # apt install imagemagick-6.q16hdri libmagickcore-6.q16-6 libmagickwand-6.q16-6 libmagickwand-6.q16hdri-6 libmagickcore-6.q16-6-extra libmagickcore-6.q16hdri-3-extra -y
+  # apt install cutecom minicom
+
   if [[ $(uname -n) == "debian" ]]; then
     cd /tmp && wget $xfce4PanelProfilesMirrorLink
     sudo apt install ./xfce4-panel-profiles*.deb
   fi
 }
 
-function installAll () {
+function installAll() {
   echo " "
   hideConfigurationItems
   sudo apt update -qq
@@ -45,7 +53,7 @@ function installAll () {
   echo " "
 }
 
-function printUsage () {
+function printUsage() {
   echo " "
   echo "USAGE: install | hide_conf_items | show_conf_items"
   echo "--------------------------------------------------"
