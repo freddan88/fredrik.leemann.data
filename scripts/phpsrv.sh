274b08e608b55cd16bdfc8198939bd6b45bb7b75
freddan88 <fredrik@leemann.se>
2022-03-20 20:30:45 +0100

    Removed unessecary code

--- scripts/phpsrv.sh
+++ scripts/phpsrv.sh
@@ -4,21 +4,14 @@
 # Website: www.leemann.se/fredrik
 # Updated: 2022-03-06
 
-web_browser=$(xdg-settings get default-web-browser)
-if [ -n "${web_browser}" ]; then
-    web_browser=$(echo $web_browser | cut -f1 -d.)
-else
-    web_browser="firefox"
-fi
-
 network_port=$PHPSRV_NETWORK_PORT
 if [ -z "${network_port}" ]; then
-    network_port="8080"
+  network_port="8080"
 fi
 
 network_ip=$PHPSRV_NETWORK_IP
 if [ -z "${network_ip}" ]; then
-    network_ip=0.0.0.0
+  network_ip=0.0.0.0
 fi
 
 ########
@@ -29,6 +22,6 @@ echo " "
 
 sleep 2
 
-$web_browser http://localhost:$network_port &
+exo-open --launch WebBrowser http://localhost:$network_port &
 
 php -S $network_ip:$network_port
