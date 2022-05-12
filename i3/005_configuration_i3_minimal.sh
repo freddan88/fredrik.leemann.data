c0f72cf264fe53cb17bc52c8c8ff04ceac6354ea
freddan88 <fredrik@leemann.se>
2022-05-12 20:40:52 +0200

    Changed names on variables

--- i3/005_configuration_i3_minimal.sh
+++ i3/005_configuration_i3_minimal.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-url_i3_config="https://raw.githubusercontent.com/freddan88/fredrik.linux.files/main/i3/configs/config_i3_minimal.txt"
-url_i3_status_config="https://raw.githubusercontent.com/freddan88/fredrik.linux.files/main/i3/configs/config_i3_status.txt"
+url_config_i3="https://raw.githubusercontent.com/freddan88/fredrik.linux.files/main/i3/configs/config_i3_minimal.txt"
+url_config_i3_status="https://raw.githubusercontent.com/freddan88/fredrik.linux.files/main/i3/configs/config_i3_status.txt"
 
 ################################
 # DO NOT EDIT BELOW THIS LINE! #
@@ -16,10 +16,9 @@ fi
 
 echo " "
 echo "UPDATING i3 CONFIG" && sleep 2
-echo " fredrik
-"
+echo " "
 rm -rf $HOME/.config/i3 && mkdir -p $HOME/.config/i3 && cd $HOME/.config/i3
-wget -O config $url_i3_config && wget -O config_i3_status $url_i3_status_config
+wget -O config $url_config_i3 && wget -O config_i3_status $url_config_i3_status
 
 echo " "
 echo "NEW i3 CONFIG-FILES IN $HOME/.config/i3" && sleep 2
