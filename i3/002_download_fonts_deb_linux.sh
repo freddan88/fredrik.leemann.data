8a4e42da24288b230e53a14e968370a9d622ffe5
freddan88 <fredrik@leemann.se>
2022-05-14 12:42:04 +0200

    Disabling and removing some fonts

--- i3/002_download_fonts_deb_linux.sh
+++ i3/002_download_fonts_deb_linux.sh
@@ -2,6 +2,7 @@
 
 url_google_fonts="https://github.com/google/fonts/archive/main.tar.gz"
 url_jetbrains_mono_fonts="https://github.com/JetBrains/JetBrainsMono/releases/download/v2.242/JetBrainsMono-2.242.zip"
+url_jetbrains_mono_fonts_nerd="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip"
 
 ################################
 # DO NOT EDIT BELOW THIS LINE! #
@@ -18,21 +19,27 @@ echo " "
 echo "INSTALLING FONTS" && sleep 2
 echo " "
 
-apt update -qq && apt install fonts-cascadia-code fonts-cantarell fonts-font-awesome -y
+apt update -qq && apt install fonts-cascadia-code fonts-cantarell -y
 
-if [ ! -d "/usr/share/fonts/truetype/jetbrains-mono" ]; then
-  cd /tmp && wget $url_jetbrains_mono_fonts && unzip -o JetBrainsMono*.zip
-  cd /tmp && mkdir -p /usr/share/fonts/truetype/jetbrains-mono
-  cd /tmp && find $PWD/fonts/ttf/ -name "*.ttf" -exec install -m644 {} /usr/share/fonts/truetype/jetbrains-mono/ \;
-fi
+# if [ ! -d "/usr/share/fonts/truetype/jetbrains-mono" ]; then
+#   cd /tmp && wget $url_jetbrains_mono_fonts && unzip -o JetBrainsMono*.zip
+#   cd /tmp && mkdir -p /usr/share/fonts/truetype/jetbrains-mono
+#   cd /tmp && find $PWD/fonts/ttf/ -name "*.ttf" -exec install -m644 {} /usr/share/fonts/truetype/jetbrains-mono/ \;
+# fi
 
 if [ ! -d "/usr/share/fonts/truetype/google-fonts" ]; then
   cd /tmp && wget $url_google_fonts && tar -zxvf main.tar.gz
   cd /tmp && mkdir -p /usr/share/fonts/truetype/google-fonts
-  cd /tmp && find $PWD/fonts-main/ -name "*.ttf" -exec sudo install -m644 {} /usr/share/fonts/truetype/google-fonts/ \;
+  cd /tmp && find $PWD/fonts-main/ -name "*.ttf" -exec install -m644 {} /usr/share/fonts/truetype/google-fonts/ \;
 fi
 
-cd /tmp && rm -rf fonts* JetBrainsMono*.zip main.tar.gz
+# if [ ! -d "/usr/share/fonts/truetype/jetbrains-mono-nerd" ]; then
+#   cd /tmp && mkdir -p jetbrains-mono-nerd && mkdir -p /usr/share/fonts/truetype/jetbrains-mono-nerd
+#   cd /tmp/jetbrains-mono-nerd && wget $url_jetbrains_mono_fonts_nerd && unzip -o JetBrainsMono*.zip
+#   cd /tmp && find $PWD/jetbrains-mono-nerd/ -name "*.ttf" -exec install -m644 {} /usr/share/fonts/truetype/jetbrains-mono-nerd \;
+# fi
+
+cd /tmp && rm -rf fonts* JetBrainsMono*.zip main.tar.gz jetbrains-mono-nerd
 
 echo " "
 echo "UPDATING FONT-CACHE" && sleep 2
