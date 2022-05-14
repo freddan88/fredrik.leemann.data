d151937f7e0a51170a66a7e6dd371789d9c1e6c2
freddan88 <fredrik@leemann.se>
2022-05-14 15:49:17 +0200

    Edited script to download new background

--- i3/003_software_i3_minimal_debian.sh
+++ i3/003_software_i3_minimal_debian.sh
@@ -22,6 +22,9 @@ if [ ! -d "/usr/share/slim" ]; then
   cd /tmp && wget https://github.com/adi1090x/slim_themes/archive/refs/heads/master.zip && unzip -o master.zip
   cd /tmp/slim_themes-master/themes && cp -rfv * /usr/share/slim/themes
   cd /tmp && rm -rf master.zip slim_themes-master
+  cd /tmp && wget https://github.com/freddan88/fredrik.linux.files/blob/main/i3/downloads/background.png
+  cd /usr/share/slim/themes/cayny && mv background.png background_old_01.png
+  cd /tmp && mv background.png /usr/share/slim/themes/cayny/background.png
 fi
 
 slimConfString=$(cat /etc/slim.conf | grep "current_theme")
