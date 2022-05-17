51b4feb5e2d334570fe133a45f8bb2d62c078770
freddan88 <fredrik@leemann.se>
2022-05-17 18:49:11 +0200

    Refactor links into variables

--- i3/003_software_i3_minimal_debian.sh
+++ i3/003_software_i3_minimal_debian.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+url_latest_slim_themes="https://github.com/adi1090x/slim_themes/archive/refs/heads/master.zip"
+url_slim_theme_cayny_background="https://github.com/freddan88/fredrik.linux.files/raw/main/i3/downloads/background.png"
+
 # Link to file on GitHub
 # https://github.com/freddan88/fredrik.linux.files/blob/main/i3/003_software_i3_minimal_debian.sh
 
@@ -25,10 +28,10 @@ apt install xorg xinput slim lxappearance arandr pulseaudio alsa-utils pavucontr
 apt autoremove -y && apt update
 
 if [ ! -d "/usr/share/slim/themes/cayny" ]; then
-  cd /tmp && wget https://github.com/adi1090x/slim_themes/archive/refs/heads/master.zip && unzip -o master.zip
+  cd /tmp && wget $url_latest_slim_themes && unzip -o master.zip
   cd /tmp/slim_themes-master/themes && cp -rfv * /usr/share/slim/themes
   cd /tmp && rm -rf master.zip slim_themes-master
-  cd /tmp && wget https://github.com/freddan88/fredrik.linux.files/raw/main/i3/downloads/background.png
+  cd /tmp && wget $url_slim_theme_cayny_background
   cd /usr/share/slim/themes/cayny && mv background.png background_old_01.png
   cd /tmp && mv background.png /usr/share/slim/themes/cayny/background.png
 fi
