aff3b6cdc0ef25b3d2f495d76a97b881ac581269
freddan88 <fredrik@leemann.se>
2022-07-16 22:40:15 +0200

    Update script remove code

--- i3/005_software_webdev_deb_linux.sh
+++ i3/005_software_webdev_deb_linux.sh
@@ -36,7 +36,7 @@ if [[ $(lsb_release -is) == "Ubuntu" ]]; then
 fi
 
 apt update -qq && apt install docker-ce docker-ce-cli containerd.io -y
-apt install apache2 libapache2-mpm-itk libapache2-mod-php libsodium23 sqlite3 sqlitebrowser install mariadb-client mariadb-server -y
+apt install apache2 libapache2-mpm-itk libapache2-mod-php libsodium23 sqlite3 sqlitebrowser mariadb-client mariadb-server -y
 apt install php php-{bcmath,cli,common,xdebug,curl,soap,gd,mbstring,mysql,opcache,readline,sqlite3,xml,zip,imagick,pear,cgi,phpseclib} -y
 apt install imagemagick imagemagick-common imagemagick-6-common imagemagick-6.q16 imagemagick-6.q16hdri libmagickcore-6.q16-6 -y
 apt install libmagickwand-6.q16-6 libmagickwand-6.q16hdri-6 libmagickcore-6.q16-6-extra libmagickcore-6.q16hdri-3-extra -y
