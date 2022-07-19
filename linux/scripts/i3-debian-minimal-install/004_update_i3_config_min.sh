f5a046fc0247835e691cb6caf109deebf454fdc1
freddan88 <fredrik@leemann.se>
2022-07-19 17:56:50 +0200

    Updating file to also download templates

--- linux/scripts/i3-debian-minimal-install/004_update_i3_config_min.sh
+++ linux/scripts/i3-debian-minimal-install/004_update_i3_config_min.sh
@@ -4,6 +4,7 @@ url_config_i3="https://raw.githubusercontent.com/freddan88/fredrik.leemann.data/
 url_config_i3_status="https://raw.githubusercontent.com/freddan88/fredrik.leemann.data/main/linux/configurations/i3/i3_status_configuration.txt"
 url_styles_i3_keybindings="https://raw.githubusercontent.com/freddan88/fredrik.leemann.data/main/linux/scripts/i3/i3keybindings.css"
 url_script_i3_keybindings="https://raw.githubusercontent.com/freddan88/fredrik.leemann.data/main/linux/scripts/i3/i3keybindings.sh"
+url_home_templates="https://github.com/freddan88/fredrik.leemann.data/raw/main/linux/templates.zip"
 
 ################################
 # DO NOT EDIT BELOW THIS LINE! #
@@ -38,6 +39,20 @@ ls -al $HOME/.config/i3
 
 # i3-msg restart 1>/dev/null
 # i3-msg reload 1>/dev/null
+
+echo " "
+echo "DOWNLOADING AND INSTALLING TEMPLATES FOR THE FILEBROWSER"
+echo " "
+
+dir_home_templates=$(xdg-user-dir TEMPLATES)
+
+cd "$dir_home_templates" && wget "$url_home_templates"
+cd "$dir_home_templates" && unzip -o templates.zip
+cd "$dir_home_templates" && rm -f templates.zip
+
+echo " "
+
+ls -al "$dir_home_templates"
 
 echo " "
 echo "DONE!"
