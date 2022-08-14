4c1339e107b0f17f8fa80fd49d0ef2f928e64899
freddan88 <fredrik@leemann.se>
2022-08-14 17:30:55 +0200

    Changed link to debian-package

--- linux/scripts/debain-minimal-install/debain_minimal_shared_software.sh
+++ linux/scripts/debain-minimal-install/debain_minimal_shared_software.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 url_google_chrome_browser="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-url_xfce_panel_profiles="http://mirrors.kernel.org/ubuntu/pool/universe/x/xfce4-panel-profiles/xfce4-panel-profiles_1.0.13-0ubuntu2_all.deb"
+url_xfce_panel_profiles="http://ftp.ports.debian.org/debian-ports/pool/main/x/xfce4-panel-profiles/xfce4-panel-profiles_1.0.13-1.1_all.deb"
 url_pulseaudio_ctl="https://github.com/graysky2/pulseaudio-ctl/archive/refs/tags/v1.70.zip"
 
 ################################
