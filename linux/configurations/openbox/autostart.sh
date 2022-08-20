e6c73c1daa9a494dfa390d43798107c318144e31
freddan88 <fredrik@leemann.se>
2022-08-20 19:02:42 +0200

    Updated configuration and script

--- linux/configurations/openbox/autostart.sh
+++ linux/configurations/openbox/autostart.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/xfce4-power-manager &
 /usr/bin/xfce4-panel &
 /usr/bin/nm-applet &
 nitrogen --restore &
 picom -b &
+xfce4-panel-profiles load "$HOME"/.local/share/xfce4-panel-profiles/openbox-xfce4-panel-01.tar.bz2
