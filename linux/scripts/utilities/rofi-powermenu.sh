e99a38f0a1fcf4bdb7fa1ce20f59307d1e59a6ba
freddan88 <fredrik@leemann.se>
2022-08-21 10:35:24 +0200

    Change rofi-powermenu

--- linux/scripts/utilities/rofi-powermenu.sh
+++ linux/scripts/utilities/rofi-powermenu.sh
@@ -1,16 +1,53 @@
 #!/usr/bin/env bash
 
-option=$(printf "Lock\nLogout\nReboot\nShutdown" | rofi -dmenu -i -theme /usr/share/rofi/themes/Pop-Dark.rasi)
+rofiTheme="/usr/share/rofi/themes/Pop-Dark.rasi"
 
 ################################
 # DO NOT EDIT BELOW THIS LINE! #
 ################################
 
+exit_option="Exit"
+lock_option="Lock"
+logout_option="Logout"
+reboot_option="Reboot"
+shutdown_option="Shutdown"
+
+print_usage() {
+  echo " "
+  echo "Usage:"
+  echo " - Running this script with no arguments will show menu (Requires rofi to be installed)"
+  echo " - Arguments: $lock_option | $logout_option | $reboot_option | $shutdown_option"
+  echo " - Example: $0 $lock_option"
+  echo " "
+}
+
+if [[ -n "$1" ]]; then
+  case "$1" in
+  "$lock_option")
+    option="$lock_option"
+    ;;
+  "$logout_option")
+    option="$logout_option"
+    ;;
+  "$reboot_option")
+    option="$reboot_option"
+    ;;
+  "$shutdown_option")
+    option="$shutdown_option"
+    ;;
+  esac
+else
+  option=$(printf "%s\n%s\n%s\n%s\n%s" "$exit_option" "$lock_option" "$logout_option" "$reboot_option" "$shutdown_option" | rofi -dmenu -i -theme $rofiTheme)
+fi
+
 case "$option" in
-"Lock")
+"$exit_option")
+  print_usage
+  ;;
+"$lock_option")
   dm-tool lock
   ;;
-"Logout")
+"$logout_option")
   wm=$(wmctrl -m | grep 'Name' | cut -d":" -f2 | xargs)
   if [ "$wm" = "Openbox" ]; then
     openbox --exit
@@ -20,13 +57,13 @@ case "$option" in
     pkill x
   fi
   ;;
-"Reboot")
+"$reboot_option")
   systemctl reboot
   ;;
-"Shutdown")
+"$shutdown_option")
   systemctl poweroff
   ;;
 *)
-  exit 1
+  print_usage
   ;;
 esac
