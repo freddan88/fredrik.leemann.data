f545e82d3158134ee3dbb1dfca672cad484a5adb
freddan88 <fredrik@leemann.se>
2023-04-08 01:41:01 +0200

    Changes in documentation for script

--- linux/scripts/utilities/feh-wallpaper-randomizer.sh
+++ linux/scripts/utilities/feh-wallpaper-randomizer.sh
@@ -6,11 +6,11 @@
 # WALLPAPER IMAGE DIRECTORY
 directory="/usr/share/wallpapers"
 
-# WALLPAPER IMAGE MODE
-# MODES: center|fill|max|scale|tile
+# WALLPAPER IMAGE MODE (MODES)
+# <center|fill|max|scale|tile>
 mode="fill"
 
-# WALLPAPER IMAGE CHANGE INTERVAL
+# WALLPAPER IMAGE CHANGE INTERVAL (SECONDS)
 interval=300
 
 # DO NOT EDIT BELOW THIS LINE
@@ -18,17 +18,17 @@ interval=300
 
 echo ""
 if [ "$(command -v feh)" ]; then
-    if [ -d "$directory" ]; then
-        while true; do
-            for image in $directory; do
-                feh --recursive --bg-$mode --randomize "$image"
-                sleep $interval
-            done
-        done
-    else
-        echo "ERROR: DIRECTORY NOT FOUND"
-    fi
+  if [ -d "$directory" ]; then
+    while true; do
+      for image in $directory; do
+        feh --recursive --bg-$mode --randomize "$image"
+        sleep $interval
+      done
+    done
+  else
+    echo "ERROR: DIRECTORY NOT FOUND"
+  fi
 else
-    echo "ERROR: COMMAND FEH NOT FOUND"
+  echo "ERROR: COMMAND FEH NOT FOUND"
 fi
 echo ""
