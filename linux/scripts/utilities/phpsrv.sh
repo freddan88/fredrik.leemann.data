441ea52a1dd9ecba2d53d1022987b9c0b9037656
freddan88 <fredrik@leemann.se>
2023-07-27 03:33:36 +0200

    updates to make it possible to kill stale instances with a command in your bash terminal

--- linux/scripts/utilities/phpsrv.sh
+++ linux/scripts/utilities/phpsrv.sh
@@ -1,34 +1,54 @@
 #!/usr/bin/env bash
 
 lan_port=8000
-lan_interface=enp6s0
-browser=google-chrome
-# browser=firefox
 
 ################################
 # DO NOT EDIT BELOW THIS LINE! #
 ################################
 
-soc1="$(ip addr | grep $lan_interface | grep inet | xargs | cut -d"/" -f1 | cut -d" " -f2):$lan_port"
-soc2=127.0.0.1:"$lan_port"
-soc3=localhost:"$lan_port"
+function stop_previous_instance {
+  previus_pid=$(netstat -ltnp 2>/dev/null | grep 0.0.0.0:$lan_port | awk '{print $7}' | cut -d'/' -f1)
 
-php -S 0.0.0.0:$lan_port >/dev/null &
-php_server_pid=$!
+  if [[ -n "${previus_pid}" ]]; then
+    kill -9 "$previus_pid"
+  fi
+}
 
-echo " "
-echo "PHP Server version 2.2.5 Linux"
-echo " "
-echo "Server listening on:"
-echo "--------------------"
+function start_new_instance {
+  php -S 0.0.0.0:$lan_port >/dev/null &
+  php_server_pid=$!
 
-echo " "
-echo "$soc1"
-echo "$soc2"
-echo "$soc3"
-echo " "
+  soc1="127.0.0.1:$lan_port"
+  soc2="localhost:$lan_port"
 
-echo "Server started on pid: $php_server_pid"
-echo " "
+  echo " "
+  echo "PHP Server version 2.5.2 Linux"
+  echo " "
+  echo "Server listening on all interfaces including:"
+  echo "---------------------------------------------"
 
-$browser http://"$soc1" 2>/dev/null
+  echo " "
+  echo "- $soc1"
+  echo "- $soc2"
+  echo " "
+
+  echo "Server started on port: $lan_port using pid: $php_server_pid"
+  echo " "
+
+  echo "You can run 'phpsrv kill' to stop stale instances of this server"
+  echo " "
+
+  xdg-open http://"$soc1" 2>/dev/null
+
+  trap stop_previous_instance EXIT
+}
+
+case $1 in
+kill)
+  stop_previous_instance
+  ;;
+*)
+  stop_previous_instance
+  start_new_instance
+  ;;
+esac
