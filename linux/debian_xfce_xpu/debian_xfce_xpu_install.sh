8b981cc515541f7d59e69af1c153b4089c8a485e
freddan88 <fredrik@leemann.se>
2023-07-29 00:00:49 +0200

    edited script for option to install snaps

--- linux/debian_xfce_xpu/debian_xfce_xpu_install.sh
+++ linux/debian_xfce_xpu/debian_xfce_xpu_install.sh
@@ -4,6 +4,8 @@
 # https://github.com/microsoft/cascadia-code/releases
 # https://www.mongodb.com/docs/compass/master/install
 
+download_snaps=false
+
 url_mongo_db_compass="https://downloads.mongodb.com/compass/mongodb-compass_1.39.0_amd64.deb"
 url_fonts_cascadia_code="https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip"
 url_fonts_ubuntu="https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip"
@@ -30,17 +32,18 @@ apt-add-repository contrib non-free -y
 apt install ttf-mscorefonts-installer unrar -y
 apt update && apt upgrade -y
 
-apt install xfce4 xfce4-goodies xfce4-panel-profiles slick-greeter lightdm-settings numlockx xinput xdotool wmctrl rofi screen members neofetch pwgen htop w3m -y
-apt install cpuid cpuidtool cpuinfo lshw ghostscript v4l-utils fzf jq net-tools fail2ban cmatrix screenkey orca onboard minicom cutecom pandoc powertop lrzsz -y
-apt install ntfs-3g dosfstools exfatprogs dos2unix cifs-utils smbclient samba nfs-common ftp tftp tftpd-hpa mariadb-client gparted stacer catfish perl baobab -y
-apt install arc-theme gnome-icon-theme elementary-xfce-icon-theme gnome-system-monitor gnome-disk-utility remmina openssl libpcre3 synaptic snapd -y
-apt install ufw gufw gimp vlc pitivi simplescreenrecorder obs-studio libreoffice mousepad thunderbird galculator imagemagick mugshot exiftool -y
+apt install xfce4 xfce4-goodies catfish mugshot xfce4-panel-profiles slick-greeter lightdm-settings numlockx xinput xdotool wmctrl rofi members neofetch -y
+apt install cpuid cpuidtool cpuinfo lshw ghostscript v4l-utils fzf jq net-tools fail2ban cmatrix screenkey orca onboard minicom cutecom pandoc powertop -y
+apt install ntfs-3g dosfstools exfatprogs dos2unix cifs-utils smbclient samba nfs-common ftp tftp tftpd-hpa mariadb-client gparted stacer perl baobab -y
+apt install ufw gufw gimp vlc pitivi simplescreenrecorder obs-studio libreoffice mousepad thunderbird galculator imagemagick exiftool pwgen htop screen -y
+apt install arc-theme gnome-icon-theme elementary-xfce-icon-theme gnome-system-monitor gnome-disk-utility remmina openssl libpcre3 synaptic w3m lrzsz -y
 apt install network-manager network-manager-gnome network-manager-openvpn network-manager-openvpn-gnome -y
 apt install ffmpeg libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi -y
 
-ln -s /etc/profile.d/apps-bin-path.sh /etc/X11/Xsession.d/99snap
-
-snap install spotify
+if [ $download_snaps ]; then
+  apt install snapd && snap install spotify
+  ln -s /etc/profile.d/apps-bin-path.sh /etc/X11/Xsession.d/99snap
+fi
 
 if [ ! -f "$(command -v google-chrome)" ]; then
   cd /tmp && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -72,10 +75,10 @@ chown -R "$SUDO_USER:$SUDO_USER" .config
 
 # apt install sqlite3
 
-snap install insomnia
-snap install dbeaver-ce
-snap install sqlitebrowser
-snap install mysql-workbench-community
+$download_snaps && snap install insomnia
+$download_snaps && snap install dbeaver-ce
+$download_snaps && snap install sqlitebrowser
+$download_snaps && snap install mysql-workbench-community
 
 if [ ! -f "$(command -v mongodb-compass)" ]; then
   cd /tmp && wget $url_mongo_db_compass
