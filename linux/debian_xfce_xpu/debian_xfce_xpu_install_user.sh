1e9f8d438e4c61e165c6eac007e9ce62e9d26575
freddan88 <fredrik@leemann.se>
2023-07-29 10:21:06 +0200

    Add new script for users

---
+++ linux/debian_xfce_xpu/debian_xfce_xpu_install_user.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+################################
+# DO NOT EDIT BELOW THIS LINE! #
+################################
+
+if [ "$SUDO_USER" ]; then
+  echo " "
+  echo "PLEASE DO NOT RUN THIS SCRIPT AS A SUDO-USER"
+  echo " "
+  exit
+fi
+
+cd "$HOME" || exit
+
+wget https://github.com/freddan88/fredrik.leemann.data/raw/main/linux/debian_xfce_xpu/debian_xfce_xpu_home.zip
+unzip -oq debian_xfce_xpu_home.zip
+rm -f debian_xfce_xpu_home.zip
