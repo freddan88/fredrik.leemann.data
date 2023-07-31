cef253bb0f4f3df2545484c808bf7fd60246589f
freddan88 <fredrik@leemann.se>
2023-07-31 02:10:21 +0200

    Edit and correct mistake

--- linux/debian_xfce_xpu/debian_xfce_xpu_install_sudo.sh
+++ linux/debian_xfce_xpu/debian_xfce_xpu_install_sudo.sh
@@ -53,7 +53,7 @@ fi
 
 if [ ! -f "$(command -v google-chrome-stable)" ]; then
   cd /tmp && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  apt install ./google-chfredrome-stable_current_amd64.deb -y
+  apt install ./google-chrome-stable_current_amd64.deb -y
   rm -f google-chrome-stable_current_amd64.deb
 fi
 
