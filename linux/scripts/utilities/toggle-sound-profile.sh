34aa530c8874693090b65f5e56c0cb358a740668
freddan88 <fredrik@leemann.se>
2023-08-05 21:53:18 +0200

    Update script used in xfce4 to change sound-output source (profile)

--- linux/scripts/utilities/toggle-sound-profile.sh
+++ linux/scripts/utilities/toggle-sound-profile.sh
@@ -5,15 +5,17 @@ sound_card="alsa_card.pci-0000_0e_00.4"
 sound_profile_01="output:analog-stereo+input:analog-stereo" # Analog 3.5mm Audio Profile
 sound_profile_02="output:iec958-stereo+input:analog-stereo" # Digital SDPIF Audio Profile
 
-################################
-# DO NOT EDIT BELOW THIS LINE! #
-################################
-
 ###########################################################################################################################
 active_profile=$(pacmd list-cards | grep -A40 $sound_card | grep 'active profile' | awk '{print $3}' | sed -r 's/[<>]+//g')
 
 if [ "$active_profile" = "$sound_profile_01" ]; then
   pactl set-card-profile $sound_card $sound_profile_02
+  current_profile=$sound_profile_02
 else
   pactl set-card-profile $sound_card $sound_profile_01
+  current_profile=$sound_profile_01
+fi
+
+if [ "$(command -v notify-send)" ]; then
+  notify-send 'Changed Sound Output Profile' $current_profile --icon=dialog-information --expire-time=500
 fi
