87b035b5bb53df4703b34da7469963116a2bc6d2
freddan88 <fredrik@leemann.se>
2023-08-06 23:26:11 +0200

    Add examples and links to script

--- linux/scripts/utilities/git-interactive.sh
+++ linux/scripts/utilities/git-interactive.sh
@@ -3,7 +3,7 @@
 untracked_files=("$(git add -A -n | cut -d" " -f2 | xargs)")
 
 for i in "${!untracked_files[@]}"; do
-    echo "'${untracked_files[$i]}' $i" 'off' '\'
+  echo "'${untracked_files[$i]}' $i" 'off' '\'
 done
 
 # selected_choices=$(dialog --no-shadow --output-fd 1 --checklist 'Files to include?' 30 60 10 \
@@ -14,3 +14,15 @@ done
 # clear
 
 # echo "$selected_choices"
+
+# LINKS
+# https://linux.die.net/man/1/dialog
+# https://linuxcommand.org/lc3_adv_dialog.php
+# https://www.geeksforgeeks.org/creating-dialog-boxes-with-the-dialog-tool-in-linux
+#
+# dialog --checklist ‘checklist’ 30 60 60 ‘apple’ 5 ‘off’ ‘banana’ 2 ‘off’ ‘coco’ 3 ‘on’ ‘delta’ 4 ‘off’ 2> dialog.txt
+# return_value=$?
+#
+# if $return_value = 0; then
+#   cat dialog.txt
+# fi
