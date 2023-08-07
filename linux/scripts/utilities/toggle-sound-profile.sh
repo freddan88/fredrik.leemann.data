a9365e1fc1db29642974ea4cbc4e5bbacc3cd372
freddan88 <fredrik@leemann.se>
2023-08-07 23:48:19 +0200

    Add example commands: pacmd

--- linux/scripts/utilities/toggle-sound-profile.sh
+++ linux/scripts/utilities/toggle-sound-profile.sh
@@ -5,6 +5,11 @@ sound_card="alsa_card.pci-0000_0e_00.4"
 sound_profile_01="output:analog-stereo+input:analog-stereo" # Analog 3.5mm Audio Profile
 sound_profile_02="output:iec958-stereo+input:analog-stereo" # Digital SDPIF Audio Profile
 
+# COMMANDS TO FIND SOUND-CARDS AND PROFILES
+# > pacmd list-sinks
+# > pacmd list-cards
+# > pacmd list-sources
+
 ###########################################################################################################################
 active_profile=$(pacmd list-cards | grep -A40 $sound_card | grep 'active profile' | awk '{print $3}' | sed -r 's/[<>]+//g')
 
