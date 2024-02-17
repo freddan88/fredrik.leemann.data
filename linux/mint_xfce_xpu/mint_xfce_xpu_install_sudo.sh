cc03713161527c654b1442b37c716c5fac8c540a
freddan88 <fredrik@leemann.se>
2024-02-18 00:40:10 +0100

    Added some packages

--- linux/mint_xfce_xpu/mint_xfce_xpu_install_sudo.sh
+++ linux/mint_xfce_xpu/mint_xfce_xpu_install_sudo.sh
@@ -50,6 +50,14 @@ function print_user_global_bin_and_exit_script() {
 echo "$HOME"
 whoami
 
+apt-get update && apt-get install zsh git gh nano vim neovim ssh zip unzip tar gzip bzip2 bzip3 7zip p7zip-full xzip fastjar lrzip -y
+apt-get install neofetch trash-cli ranger thefuck tldr rofi tmux tree exa bat ripgrep xdotool wmctrl members fzf zoxide entr mc lshw -y
+apt-get install dos2unix cifs-utils smbclient samba nfs-common ftp tftp tftpd-hpa gparted httpie httping perl curl htop powertop nmap -y
+apt-get install spotify-client network-manager-openvpn-gnome catfish mugshot dbus-x11 gimp vlc pitivi simplescreenrecorder obs-studio -y
+apt-get install gnome-system-monitor gnome-disk-utility stacer baobab rsync libsodium23 ffmpeg pwgen imagemagick exiftool ghostscript -y
+apt-get install pandoc lrzsz minicom cutecom remmina thunderbird orca onboard screenkey xinput numlockx synaptic ufw gufw openssl -y
+apt-get install arc-theme elementary-xfce-icon-theme cmatrix screen lsb-release -y
+
 mkdir ~/.gnupg
 chmod 700 ~/.gnupg
 chmod 600 ~/.gnupg/*
