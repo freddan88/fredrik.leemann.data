f3dfeeb094dc64ba6712652d921a86c256b850b4
freddan88 <fredrik@leemann.se>
2024-02-18 18:42:23 +0100

    Add configuration-files

---
+++ linux/configurations/vscode/vscode_extensions_list.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension DEVSENSE.composer-php-vscode
+code --install-extension DEVSENSE.phptools-vscode
+code --install-extension DEVSENSE.profiler-php-vscode
+code --install-extension donjayamanne.githistory
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension file-icons.file-icons
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension foxundermoon.shell-format
+code --install-extension mads-hartmann.bash-ide-vscode
+code --install-extension mikestead.dotenv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension naumovs.color-highlight
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension Remisa.shellman
+code --install-extension ritwickdey.LiveServer
+code --install-extension rogalmic.bash-debug
+code --install-extension rpinski.shebang-snippets
+code --install-extension sleistner.vscode-fileutils
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension timonwong.shellcheck
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension yoavbls.pretty-ts-errors
