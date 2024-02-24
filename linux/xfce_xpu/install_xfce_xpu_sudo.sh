7418acea88d53815b3aa3de250dc74377dc15bdc
freddan88 <fredrik@leemann.se>
2024-02-24 14:31:11 +0100

    Corrected script

--- linux/xfce_xpu/install_xfce_xpu_sudo.sh
+++ linux/xfce_xpu/install_xfce_xpu_sudo.sh
@@ -59,7 +59,7 @@ echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.
 
 apt-get update
 
-apt-get install zsh ssh git nano vim neovim lsb-releasezip ca-certificates unzip tar gzip bzip2 ristretto remmina -y
+apt-get install zsh ssh git nano vim neovim lsb-release ca-certificates zip unzip tar gzip bzip2 ristretto remmina -y
 apt-get install arc-theme elementary-xfce-icon-theme cmatrix screen xfce4-panel-profiles keepassxc stacer baobab rsync -y
 apt-get install pandoc lrzsz minicom cutecom remmina thunderbird orca onboard screenkey xinput numlockx synaptic ufw gufw openssl -y
 apt-get install neofetch trash-cli ranger thefuck tldr rofi tmux tree exa bat ripgrep xdotool wmctrl members fzf zoxide entr mc lshw -y
