22d0951e12bcaf3112c82b43f538702652ea7ab6
freddan88 <fredrik@leemann.se>
2024-02-25 16:39:24 +0100

    Commented code to let it work for ubuntu and linux mint

--- linux/xfce_xpu/install_xfce_xpu_sudo.sh
+++ linux/xfce_xpu/install_xfce_xpu_sudo.sh
@@ -218,16 +218,12 @@ if $install_docker; then
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   chmod a+r /etc/apt/keyrings/docker.asc
 
-  distro_name=$(cat /etc/os-release | grep -w NAME | cut -d"=" -f2)
-  if [ "$distro_name" = '"Linux Mint"' ]; then
-    codename=$(cat /etc/os-release | grep -w UBUNTU_CODENAME | cut -d"=" -f2)
-    docker_source="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $codename stable"
-    echo "$docker_source" | tee /etc/apt/sources.list.d/docker.list >/dev/null
-  else
-    echo ""
-    echo "INSTALL DOCKER FOR UBUNTU"
-    echo ""
-  fi
+  # distro_name=$(cat /etc/os-release | grep -w NAME | cut -d"=" -f2)
+  # if [ "$distro_name" = '"Linux Mint"' ]; then
+  #fi
+  codename=$(cat /etc/os-release | grep -w UBUNTU_CODENAME | cut -d"=" -f2)
+  docker_source="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $codename stable"
+  echo "$docker_source" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
   apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
