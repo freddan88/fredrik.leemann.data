21b2d92e879fd55e0d30520a37b5d6f41a99dad2
freddan88 <fredrik@leemann.se>
2024-03-02 14:32:10 +0100

    Edited script to set volume when you toggle

--- linux/scripts/utilities/toggle-sound-profile.sh
+++ linux/scripts/utilities/toggle-sound-profile.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-sound_card="alsa_card.pci-0000_0e_00.4"
-
-sound_profile_01="output:analog-stereo+input:analog-stereo" # Analog 3.5mm Audio Profile
-sound_profile_02="output:iec958-stereo+input:analog-stereo" # Digital SDPIF Audio Profile
-
 # COMMANDS TO FIND SOUND-CARDS AND PROFILES
 # > pacmd list-sinks
 # > pacmd list-cards
 # > pacmd list-sources
 
+sound_card="alsa_card.pci-0000_0e_00.4"
+
+sound_profile_01="output:analog-stereo+input:analog-stereo" # Analog 3.5mm Audio Profile
+sound_profile_02="output:iec958-stereo+input:analog-stereo" # Digital SDPIF Audio Profile
+
 ###########################################################################################################################
 active_profile=$(pacmd list-cards | grep -A40 $sound_card | grep 'active profile' | awk '{print $3}' | sed -r 's/[<>]+//g')
 
 if [ "$active_profile" = "$sound_profile_01" ]; then
+  pactl set-sink-volume @DEFAULT_SINK@ 100% 2>/dev/null
   pactl set-card-profile $sound_card $sound_profile_02
   current_profile=$sound_profile_02
 else
+  pactl set-sink-volume @DEFAULT_SINK@ 25% 2>/dev/null
   pactl set-card-profile $sound_card $sound_profile_01
   current_profile=$sound_profile_01
 fi
